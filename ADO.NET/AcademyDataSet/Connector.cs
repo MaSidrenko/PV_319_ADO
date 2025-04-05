4bb4aed5a949662acfc255130e49b08cbba7b1fe
Maxim Sidorenko <Maxim Sidorenko@DESKTOP-IIFDPU3>
2025-04-05 17:09:25 +0300

    DataSet Optimize DONE

---
+++ ADO.NET/AcademyDataSet/Connector.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyDataSet
+{
+	internal class Connector
+	{
+		readonly string CONNECTION_STRING;
+		SqlConnection conn;
+		public Connector(string connectionString)
+		{
+			CONNECTION_STRING = connectionString;
+			conn = new SqlConnection(CONNECTION_STRING);
+			AllocConsole();
+			Console.WriteLine(CONNECTION_STRING);
+		}
+		~Connector()
+		{
+			FreeConsole();
+		}
+
+		public void LoadTable(string cmd, string table_name, Dictionary<int, string> table_columns)
+		{
+			try
+			{
+				DataSet dataSet = new DataSet();
+				DataTable dataTable = new DataTable();
+
+				dataTable.TableName = table_name;
+				dataSet.Tables.Add(dataTable);
+
+				DataTableReader reader = GetReader(cmd, table_name);
+
+				dataSet.Load(reader, LoadOption.OverwriteChanges, dataTable);
+
+				Console.WriteLine("========================================================================================================================");
+				foreach (DataColumn columns in dataTable.Columns)
+				{
+					Console.Write((columns).ToString().PadRight(40));
+				}
+				Console.WriteLine();
+				Console.WriteLine("========================================================================================================================");
+				foreach (DataRow row in dataTable.Rows)
+				{
+					foreach (KeyValuePair<int, string> rows in table_columns)
+					{
+						Console.Write($"{row[rows.Value]}".PadRight(40));
+					}
+					Console.WriteLine();
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+		private DataTableReader GetReader(string cmd, string name)
+		{
+			DataTable table = null;
+			try
+			{
+				table = new DataTable(name);
+				SqlDataAdapter adapter = new SqlDataAdapter(cmd, conn);
+				adapter.Fill(table);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			return table.CreateDataReader();
+		}
+		[DllImport("kernel32.dll")]
+		public static extern bool AllocConsole();
+		[DllImport("kernel32.dll")]
+		public static extern bool FreeConsole();
+	}
+}
+
--- ADO.NET/AcademyDataSet/MainForm.cs
+++ ADO.NET/AcademyDataSet/MainForm.cs
@@ -18,73 +18,31 @@ namespace AcademyDataSet
 	public partial class MainForm : Form
 	{
 		readonly string CONN_STR = "";
-		DataSet GroupsRelatedData = null;
-		SqlConnection conn = null;
+		Dictionary<int, string> d_groups = new Dictionary<int, string>
+	{
+		//{1, "group_id"},
+		{1, "Группа"},
+		{2, "Дисциплина"},
+		{3, "Учебные дни" },
+		{4, "Время начала занятий" }
+	};
+		Dictionary<int, string> d_directions = new Dictionary<int, string>
+	{
+		{1, "direction_id" },
+		{2, "direction_name" }
+	};
 		public MainForm()
 		{
 			InitializeComponent();
 			AllocConsole();
 			CONN_STR = ConfigurationManager.ConnectionStrings["PV_319_IMPORT"].ConnectionString;
-			conn = new SqlConnection(CONN_STR);
-			LoadGroupsRelatedData();
+			Connector connector = new Connector(CONN_STR);
+			connector.LoadTable("SELECT group_name AS N'Группа', direction_name AS N'Дисциплина', weekdats AS N'Учебные дни', start_time AS N'Время начала занятий' FROM Groups, Directions WHERE direction=direction_id", "Groups", d_groups);
+			Console.WriteLine("\n|-----------------------------------------------------------------------------------------------------------------------------------------------------------------|\n");
+			connector.LoadTable("SELECT * FROM Directions", "Directions", d_directions);
+			Console.WriteLine("\n|-----------------------------------------------------------------------------------------------------------------------------------------------------------------|\n");
 		}
 		
-		void LoadGroupsRelatedData()
-		{
-			Console.WriteLine(nameof(GroupsRelatedData));
-			GroupsRelatedData = new DataSet(nameof(GroupsRelatedData));
-
-			const string dsTabe_Directions = "Directions";
-			const string dst_col_direction_id = "direction_id";
-			const string dst_col_direction_name = "direction_name"
-				;
-			GroupsRelatedData.Tables.Add(dsTabe_Directions);
-			GroupsRelatedData.Tables[dsTabe_Directions].Columns.Add(dst_col_direction_id, typeof(byte));
-			GroupsRelatedData.Tables[dsTabe_Directions].Columns.Add(dst_col_direction_name, typeof(string));
-			GroupsRelatedData.Tables[dsTabe_Directions].PrimaryKey = new DataColumn[]
-				{ GroupsRelatedData.Tables[dsTabe_Directions].Columns[dst_col_direction_id] };
-
-			const string dsTable_Groups = "Groups";
-			const string dst_col_group_id = "group_id";
-			const string dst_col_group_name = "group_name";
-			const string dst_col_direction = "direction";
-			GroupsRelatedData.Tables.Add(dsTable_Groups);
-			GroupsRelatedData.Tables[dsTable_Groups].Columns.Add(dst_col_group_id, typeof(int));
-			GroupsRelatedData.Tables[dsTable_Groups].Columns.Add(dst_col_group_name, typeof(string));
-			GroupsRelatedData.Tables[dsTable_Groups].Columns.Add(dst_col_direction, typeof(byte));
-			GroupsRelatedData.Tables[dsTable_Groups].PrimaryKey = new DataColumn[]
-			{
-				GroupsRelatedData.Tables[dsTable_Groups].Columns[dst_col_group_id]
-			};
-			string dsRealtion_Groups_Directions = "GroupsDirections";
-			GroupsRelatedData.Relations.Add
-				(
-					dsRealtion_Groups_Directions,
-					GroupsRelatedData.Tables["Directions"].Columns["direction_id"],
-					GroupsRelatedData.Tables["Groups"].Columns["direction"]
-				);
-
-			string directions_cmd = "SELECT * FROM Directions";
-			string groups_cmd = "SELECT * FROM Groups";
-			SqlDataAdapter directionsAdapter = new SqlDataAdapter(directions_cmd, conn);
-			SqlDataAdapter groupsAdapter = new SqlDataAdapter(groups_cmd, conn);
-
-			conn.Open();
-			directionsAdapter.Fill(GroupsRelatedData.Tables[dsTabe_Directions]);
-			groupsAdapter.Fill(GroupsRelatedData.Tables[dsTable_Groups]);
-			conn.Close();
-
-
-			foreach (DataRow row in GroupsRelatedData.Tables[dsTabe_Directions].Rows)
-			{
-				Console.WriteLine($"{row[dst_col_direction_id]}\t{row[dst_col_direction_name]}");
-			}
-			Console.WriteLine("\n------------------------------------------------------------\n");
-			foreach (DataRow row in GroupsRelatedData.Tables[dsTable_Groups].Rows)
-			{
-				Console.WriteLine($"{row[dst_col_group_id]}\t{row[dst_col_group_name]}\t{row.GetParentRow(dsRealtion_Groups_Directions)[dst_col_direction_name]}");
-			}
-		}
 		[DllImport("kernel32.dll")]
 		public static extern bool AllocConsole();
 		[DllImport("kernel32.dll")]
