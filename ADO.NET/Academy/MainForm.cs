72fe2f976037bd1116f0aa952fd6d7788f164c05
Maxim Sidorenko <Maxim Sidorenko@DESKTOP-IIFDPU3>
2025-04-17 20:25:48 +0300

    Parent Combo-Depeendencies DONE

--- ADO.NET/Academy/MainForm.cs
+++ ADO.NET/Academy/MainForm.cs
@@ -20,7 +20,8 @@ namespace Academy
 		public Dictionary<string, int> d_directions;
 		public Dictionary<string, int> d_groups;
 
-		public Dictionary<ComboBox, List<ComboBox>> d_dependencies;
+		public Dictionary<ComboBox, List<ComboBox>> d_children;
+		public Dictionary<ComboBox, List<ComboBox>> d_parents;
 
 		DataGridView[] tables;
 		Query[] queries = new Query[]
@@ -59,10 +60,15 @@ namespace Academy
 		public MainForm()
 		{
 			InitializeComponent();
-			d_dependencies = new Dictionary<ComboBox, List<ComboBox>>()
+			d_children = new Dictionary<ComboBox, List<ComboBox>>()
 			{
 				{cbStudentsDirection, new List<ComboBox> { cbStudentsGroup } }
 			};
+			d_parents = new Dictionary<ComboBox, List<ComboBox>>()
+			{
+				{cbStudentsGroup, new List<ComboBox> { cbStudentsDirection } }
+			};
+
 			tables = new DataGridView[]
 			{
 				dgvStudents,
@@ -81,7 +87,6 @@ namespace Academy
 			d_groups = connector.GetDictionary("group_id, group_name", "Groups");
 
 
-			//dgvStudents.DataSource = connector.Select("*", "Students");
 			dgvStudents.DataSource = connector.Select("FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name) AS N'ФИО Студента', birth_date, group_name, direction_name",
 																	"Students, Groups, Directions"
 																	, "[group]=group_id AND direction=direction_id");
@@ -100,56 +105,15 @@ namespace Academy
 			cbStudentsDirection.SelectedIndex = cbStudentsGroup.SelectedIndex = 0;
 			cbGroupsDirection.SelectedIndex = 0;
 		}
-
 		void LoadPage(int i, Query query = null)
 		{
 			if (query == null)
 				query = queries[i];
 			tables[i].DataSource = connector.Select(query.Columns, query.Tables, query.Condition, query.Group_by);
 			toolStripStatusLabelCount.Text = satusMessages[i] + CountRecordsInDGV(tables[i]);
-
 		}
-
 		private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			/*Console.WriteLine(tabControl.SelectedIndex);
-			switch (tabControl.SelectedIndex)
-			{
-				case 0:
-					{
-						dgvStudents.DataSource = connector.Select("FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name) AS N'ФИО Студента', birth_date, group_name, direction_name",
-																	"Students, Groups, Directions"
-																	, "[group]=group_id AND direction=direction_id");
-						toolStripStatusLabelCount.Text = "Количество студентов: " + (dgvStudents.RowCount - 1);
-					}
-					break;
-				case 1:
-					{
-						dgvGroups.DataSource = connector.Select("group_name,dbo.GetLearningDays(group_name) AS weekdays, start_time, direction_name", "Groups, Directions", "direction=direction_id");
-						toolStripStatusLabelCount.Text = "Количество групп: " + CountRecordsInDGV(dgvGroups);
-					}
-					break;
-				case 2:
-					{
-						//dgvDirections.DataSource = connector.Select("direction_name, COUNT(DISTINCT group_id) AS N'Количество групп', COUNT(DISTINCT stud_id) AS N'Количество студентов'", "Students, Groups, Directions", "[group]=group_id AND direction=direction_id", "direction_name");
-						dgvDirections.DataSource = connector.Select("direction_name, COUNT(DISTINCT group_id) AS N'Количество групп', COUNT(DISTINCT stud_id) AS N'Количество студентов'", "Students  RIGHT JOIN Groups ON ([group]=group_id) RIGHT JOIN Directions ON (direction=direction_id)", "", "direction_name");
-						toolStripStatusLabelCount.Text = "Количество направлений: " + (dgvDirections.RowCount - 1);
-					}
-					break;
-				case 3:
-					{
-						dgvDisciplines.DataSource = connector.Select("*", "Disciplines");
-						toolStripStatusLabelCount.Text = "Количество дисциплин: " + (dgvDisciplines.RowCount - 1);
-					}
-					break;
-				case 4:
-					{
-						dgvTeachers.DataSource = connector.Select("*", "Teachers");
-						toolStripStatusLabelCount.Text = "Количество преподавателей: " + (dgvTeachers.RowCount - 1);
-					}
-					break;
-			}*/
-			//int i = tabControl.SelectedIndex;
 			LoadPage(tabControl.SelectedIndex);
 		}
 		int CountRecordsInDGV(DataGridView dgv)
@@ -173,15 +137,32 @@ namespace Academy
 					i == 0 ? "" : $"[{cb_suffix.ToLower()}]={dictionary[(sender as ComboBox).SelectedItem.ToString()]}");
 				cbStudentsGroup.Items.Clear();
 				cbStudentsGroup.Items.AddRange(d_groups.Select(g => g.Key).ToArray());*/
-			if (d_dependencies.ContainsKey(sender as ComboBox))
+			if (d_children.ContainsKey(sender as ComboBox))
 			{
-				foreach (ComboBox cb in d_dependencies[sender as ComboBox])
+				foreach (ComboBox cb in d_children[sender as ComboBox])
 				{
 					GetDependetData(cb, (sender as ComboBox));
 				}
 			}
 			Query query = new Query(queries[tabControl.SelectedIndex]);
-			string condition = i <= 0 || (sender as ComboBox).SelectedItem == null ? "" : $"[{cb_suffix.ToLower()}]={dictionary[$"{(sender as ComboBox).SelectedItem}"]}";
+			string condition = i == 0 || (sender as ComboBox).SelectedItem == null ? "" : $"[{cb_suffix.ToLower()}]={dictionary[$"{(sender as ComboBox).SelectedItem}"]}";
+			string parent_condition = "";
+			if (d_parents.ContainsKey(sender as ComboBox))
+			{
+				foreach (ComboBox cb in d_parents[sender as ComboBox])
+				{
+					if (cb.SelectedItem != null && cb.SelectedIndex > 0)
+					{
+						string column_name = cb.Name.Substring(Array.FindLastIndex<Char>(cb.Name.ToCharArray(), Char.IsUpper));
+						string parent_dictionary_name = $"d_{column_name.ToLower()}s";
+						Dictionary<string, int> parent_dictionary = this.GetType().GetField(parent_dictionary_name).GetValue(this) as Dictionary<string, int>;
+						if (parent_condition != "")
+							parent_condition += $" AND ";
+						parent_condition += $"[{column_name}]={parent_dictionary[cb.SelectedItem.ToString()]}";
+					}
+				}
+			}
+
 			if (query.Condition == "")
 			{
 				query.Condition = condition;
@@ -189,6 +170,14 @@ namespace Academy
 			else if (condition != "")
 			{
 				query.Condition += $" AND {condition}";
+			}
+			if (query.Condition == "")
+			{
+				query.Condition = parent_condition;
+			}
+			else if (parent_condition != "")
+			{
+				query.Condition += $" AND {parent_condition}";
 			}
 			LoadPage(tabControl.SelectedIndex, query);
 		}
@@ -205,19 +194,22 @@ namespace Academy
 					$"{dependent_root.ToLower()}_id,{dependent_root.ToLower()}_name",
 					$"{dependent_root}s,{determinant_root}s",
 					determinant.SelectedItem == null || determinant.SelectedIndex <= 0 ? "" : $"{determinant_root.ToLower()}={determinant.SelectedIndex}"
-				);  
+				);
 
-			foreach(KeyValuePair<string, int> kvp in dictionary)
+			foreach (KeyValuePair<string, int> kvp in dictionary)
 			{
 				Console.WriteLine(kvp.Key + "\t" + kvp.Value);
 			}
 			//this.GetType().GetField($"d_{dependent_root}").GetValue(this) as
+
 			dependent.Items.Clear();
 			dependent.Items.AddRange(dictionary.Select(g => g.Key).ToArray());
+			dependent.Items.Insert(0, "Все");
+			dependent.SelectedIndex = 0;
+
 			Console.WriteLine(dependent_root);
 			Console.WriteLine(determinant_root);
 			Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		}
-
 	}
 }
